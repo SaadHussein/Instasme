eb83684a70e0417ae2eaebc7ecd7f6694aca23c7
SaadHussein <sa3dhusseein@gmail.com>
2024-06-28 19:49:57 +0300

    edits

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -17,24 +17,23 @@ import { BsCartPlus } from "react-icons/bs";
 
 const MainPage = () => {
 	return (
-		<Suspense fallback={<p>Loading...</p>}>
-			<div
-				className={`app-main flex-column flex-row-fluid" id="kt_app_main ${classes.mainApp}`}
-			>
-				<div className="d-flex flex-column flex-column-fluid">
-					<div id="kt_app_content" className="app-content flex-column-fluid">
-						<div
-							id="kt_app_content_container"
-							class="app-container container-fluid"
-						>
-							<div className={`${mainPageClasses.selectWidth} mb-6`}>
-								<Select
-									label={"Select a Warehouse"}
-									options={["Warehouse One", "Warehouse Two"]}
-								/>
-							</div>
-							<div className="row mb-6">
-								{/* <StatisticsCard
+		<div
+			className={`app-main flex-column flex-row-fluid" id="kt_app_main ${classes.mainApp}`}
+		>
+			<div className="d-flex flex-column flex-column-fluid">
+				<div id="kt_app_content" className="app-content flex-column-fluid">
+					<div
+						id="kt_app_content_container"
+						class="app-container container-fluid"
+					>
+						<div className={`${mainPageClasses.selectWidth} mb-6`}>
+							<Select
+								label={"Select a Warehouse"}
+								options={["Warehouse One", "Warehouse Two"]}
+							/>
+						</div>
+						<div className="row mb-6">
+							{/* <StatisticsCard
 								amount={"1,820.00"}
 								icon={<HiOutlineShoppingCart size={30} color="#1B84FF" />}
 								link={"/"}
@@ -58,61 +57,60 @@ const MainPage = () => {
 								link={"/"}
 								title={"Purchases Return"}
 							/> */}
-								<Statistics
-									title="Sales"
-									number="1780.00"
-									icon={<HiOutlineShoppingCart size={50} color="white" />}
-									backgroundColor="#3bc0c3"
-									link={"/sales/all-sales"}
-								/>
-								<Statistics
-									title="Purchases"
-									number="1160.00"
-									backgroundColor="#716cb0"
-									icon={<BsCartPlus size={50} color="white" />}
-									link={"/purchases/all-purchases"}
-								/>
-								<Statistics
-									title="Purchases Return"
-									number="0.00"
-									backgroundColor="#33b0e0"
-									icon={<IoArrowUndoOutline size={50} color="white" />}
-									link={"/sales-return"}
-								/>
-								<Statistics
-									title="Sales Return"
-									number="0.00"
-									backgroundColor="#f24f7c"
-									icon={<IoArrowRedoOutline size={50} color="white" />}
-									link={"/purchases-return"}
-								/>
-							</div>
-							<div className="row g-5 g-xl-10 g-xl-10">
-								<MultiBarGraph />
-								<PieChart
-									chartTilte={"Top Selling Products (2024)"}
-									dataFor={"topProducts"}
-								/>
-							</div>
-							<div className="row g-5 g-xl-10 g-xl-10 mt-1">
-								<StockReport />
-								<TopSellingProduct />
-							</div>
-							<div className="row g-5 g-xl-10 g-xl-10 mt-1 mb-10">
-								<XYChart />
-								<PieChart
-									chartTilte={"Top 5 Customers (June)"}
-									dataFor={"customers"}
-								/>
-							</div>
-							<div className="row g-5 g-xl-10 g-xl-10 mt-1">
-								<SalesReport />
-							</div>
+							<Statistics
+								title="Sales"
+								number="1780.00"
+								icon={<HiOutlineShoppingCart size={50} color="white" />}
+								backgroundColor="#3bc0c3"
+								link={"/sales/all-sales"}
+							/>
+							<Statistics
+								title="Purchases"
+								number="1160.00"
+								backgroundColor="#716cb0"
+								icon={<BsCartPlus size={50} color="white" />}
+								link={"/purchases/all-purchases"}
+							/>
+							<Statistics
+								title="Purchases Return"
+								number="0.00"
+								backgroundColor="#33b0e0"
+								icon={<IoArrowUndoOutline size={50} color="white" />}
+								link={"/sales-return"}
+							/>
+							<Statistics
+								title="Sales Return"
+								number="0.00"
+								backgroundColor="#f24f7c"
+								icon={<IoArrowRedoOutline size={50} color="white" />}
+								link={"/purchases-return"}
+							/>
+						</div>
+						<div className="row g-5 g-xl-10 g-xl-10">
+							<MultiBarGraph />
+							<PieChart
+								chartTilte={"Top Selling Products (2024)"}
+								dataFor={"topProducts"}
+							/>
+						</div>
+						<div className="row g-5 g-xl-10 g-xl-10 mt-1">
+							<StockReport />
+							<TopSellingProduct />
+						</div>
+						<div className="row g-5 g-xl-10 g-xl-10 mt-1 mb-10">
+							<XYChart />
+							<PieChart
+								chartTilte={"Top 5 Customers (June)"}
+								dataFor={"customers"}
+							/>
+						</div>
+						<div className="row g-5 g-xl-10 g-xl-10 mt-1">
+							<SalesReport />
 						</div>
 					</div>
 				</div>
 			</div>
-		</Suspense>
+		</div>
 	);
 };
 
--- src/pages/Sales/POS/Pos.jsx
+++ src/pages/Sales/POS/Pos.jsx
@@ -6,26 +6,24 @@ import PosOrder from "../../../components/PosComponents/PosOrder/PosOrder";
 
 const Pos = () => {
 	return (
-		<Suspense fallback={<p>Loading...</p>}>
-			<div
-				className={`app-main flex-column flex-row-fluid" id="kt_app_main ${classes.mainApp}`}
-			>
-				<div className="d-flex flex-column flex-column-fluid">
-					<PosToolbar />
-					<div id="kt_app_content" className="app-content flex-column-fluid">
-						<div
-							id="kt_app_content_container"
-							class="app-container container-fluid"
-						>
-							<div className="d-flex flex-column flex-xl-row">
-								<PosItems />
-								<PosOrder />
-							</div>
+		<div
+			className={`app-main flex-column flex-row-fluid" id="kt_app_main ${classes.mainApp}`}
+		>
+			<div className="d-flex flex-column flex-column-fluid">
+				<PosToolbar />
+				<div id="kt_app_content" className="app-content flex-column-fluid">
+					<div
+						id="kt_app_content_container"
+						class="app-container container-fluid"
+					>
+						<div className="d-flex flex-column flex-xl-row">
+							<PosItems />
+							<PosOrder />
 						</div>
 					</div>
 				</div>
 			</div>
-		</Suspense>
+		</div>
 	);
 };
 
