9648cd8a93be36e01d8ae5b9156c9759b834a51a
SaadHussein <sa3dhusseein@gmail.com>
2024-07-15 01:47:55 +0300

    finish list deposits  page

--- src/components/Accounting/ListDeposite/ListDepositeTable.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListDepositeTable = () => {
-	return <div>ListDepositeTable</div>;
-};
-
-export default ListDepositeTable;
--- src/pages/Accounting/ListDeposite/ListDeposite.jsx
+++ src/pages/Accounting/ListDeposite/ListDeposite.jsx
@@ -1,70 +1,157 @@
-import React from "react";
+import React, { useState } from "react";
 import Toolbar from "../Toolbar";
 import classes from "../../../styles/global.module.css";
-import { FaSearch } from "react-icons/fa";
-import Input from "../../../components/Global/Input/Input";
-import Button from "../../../components/Global/Button/Button";
-import Select from "../../../components/Global/Select/Select";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import privateClasses from "./ListDeposite.module.css";
 import LoadingWrapper from "../../../components/Global/LoadingWrapper/LoadingWrapper";
+import PagesNavigationBar from "../../../components/Global/PagesNavigationBar/PagesNavigationBar";
+import SearchInputIvonInside from "../../../components/Global/SearchInputIvonInside/SearchInputIvonInside";
+import TablePagination from "../../../components/Global/TablePagination/TablePagination";
+import LabelHeaderReactSelect from "../../../components/Global/LabelHeaderReactSelect/LabelHeaderReactSelect";
+import ColumnVisibility from "../../../components/Global/ColumnVisibility/ColumnVisibility";
+import ExportButtonAndList from "../../../components/Global/Export/ExportButtonAndList";
+import DateRange from "../../../components/Global/DateRange/DateRange";
+import { BsFiles } from "react-icons/bs";
+import { VscNewFile } from "react-icons/vsc";
+import ListDepositeTable from "../../../components/Accounting/ListDeposite/ListDepositeTable/ListDepositeTable";
+
+const AccountingPage = [
+	{
+		title: "List Accounts",
+		link: "/accounting/list-accounts",
+		icon: <BsFiles className="fs-2 me-2" />,
+	},
+	{
+		title: "Transfer Money",
+		link: "/accounting/transfer-money",
+		icon: <BsFiles className="fs-2 me-2" />,
+	},
+	{
+		title: "Create Expense",
+		link: "/accounting/create-expense",
+		icon: <VscNewFile className="fs-2 me-2" />,
+	},
+	{
+		title: "All Expenses",
+		link: "/accounting/all-expenses",
+		icon: <BsFiles className="fs-2 me-2" />,
+	},
+	{
+		title: "Create Deposite",
+		link: "/accounting/create-deposite",
+		icon: <VscNewFile className="fs-2 me-2" />,
+	},
+	{
+		title: "List Deposite",
+		link: "/accounting/list-deposite",
+		icon: <BsFiles className="fs-2 me-2" />,
+	},
+	{
+		title: "Expense Category",
+		link: "/accounting/expense-category",
+		icon: <BsFiles className="fs-2 me-2" />,
+	},
+	{
+		title: "Deposite Category",
+		link: "/accounting/deposite-category",
+		icon: <BsFiles className="fs-2 me-2" />,
+	},
+];
+
+const columnsTable = [
+	{ label: "Date", visible: true },
+	{ label: "Reference", visible: true },
+	{ label: "Amount", visible: true },
+	{ label: "Category", visible: true },
+	{ label: "Account", visible: true },
+	{ label: "Details", visible: true },
+	{ label: "Actions", visible: true },
+];
+
+const listDepositeTable = [
+	{
+		date: "07-07-2024",
+		reference: "DEP_1111",
+		amount: 5000.0,
+		category: "Advertising and Marketing",
+		account: "Bank 1",
+		details: "Deposit to Bank 1",
+	},
+];
 
 const ListDeposite = () => {
+	const [columns, setColumns] = useState([...columnsTable]);
+
+	const changeLabelVisiblity = (label) => {
+		const updatedColumns = columns.map((column) =>
+			column.label === label ? { ...column, visible: !column.visible } : column
+		);
+
+		setColumns([...updatedColumns]);
+	};
+
 	return (
 		<LoadingWrapper>
 			<div
 				className={`app-main flex-column flex-row-fluid" id="kt_app_main ${classes.mainApp}`}
 			>
 				<div className="d-flex flex-column flex-column-fluid">
-					<Toolbar
-						title={"List Deposite"}
-						path={["Accounting", "List Deposite"]}
-					/>
+					{/* <PagesNavigationBar pages={ProductsPage} /> */}
+					<div
+						className={`d-flex align-items-center justify-content-between ${privateClasses.navPages}`}
+					>
+						<Toolbar
+							title={"All Products"}
+							path={["Products", "All Products"]}
+						/>
+						<PagesNavigationBar pages={AccountingPage} />
+					</div>
 					<div id="kt_app_content" className="app-content flex-column-fluid">
 						<div
 							id="kt_app_content_container"
-							class="app-container container-fluid"
+							className="app-container container-fluid"
 						>
 							<div
-								className={`d-flex align-items-center justify-content-between mb-8 ${privateClasses.tableHeader}`}
+								className={`d-flex align-items-start justify-content-between mb-8 gap-2 ${privateClasses.tableHeader}`}
 							>
-								<div className="d-flex align-items-center justify-content-start gap-4">
-									<FaSearch size={24} />
-									<Input placeholder={"Search This Table"} />
-								</div>
-								<div className="d-flex align-items-center justify-content-end gap-2">
-									<Button text={"Filter"} color="" />
-									<Button text={"PDF"} />
-									<Button text={"EXCEL"} />
-									<Button text={"Create"} />
+								<SearchInputIvonInside />
+								<div
+									className={`d-flex align-items-center justify-content-end gap-2 ${privateClasses.tableButtons}`}
+								>
+									<DateRange />
+									<ExportButtonAndList />
+									<ColumnVisibility
+										columns={columns}
+										changeLabelVisiblity={changeLabelVisiblity}
+									/>
 								</div>
 							</div>
-							{/* <AdjustmentTable /> */}
-							<div className="d-flex align-items-center justify-content-between">
-								<div className="d-flex align-items-center justify-content-start w-50 gap-3">
-									<p className={`${privateClasses.numberPerPage}`}>
-										Rows Per Page:{" "}
-									</p>
+							<ListDepositeTable
+								columnsVisible={columns}
+								listDepositeTable={listDepositeTable}
+							/>
+							<div
+								className={`d-flex align-items-center justify-content-between ${privateClasses.tableFooter}`}
+							>
+								<div
+									className={`d-flex align-items-center justify-content-start w-50 gap-3 ${privateClasses.rowSelect}`}
+								>
 									<div className="w-150px">
-										<Select defaultValue="10" options={[10, 20, 30, 40, 50]} />
+										<LabelHeaderReactSelect
+											defaultValue={10}
+											label={""}
+											name={"Rows-Per-Page"}
+											placeholder={10}
+											options={[
+												{ value: 10, label: 10 },
+												{ value: 20, label: 20 },
+												{ value: 30, label: 30 },
+												{ value: 40, label: 40 },
+												{ value: 50, label: 50 },
+											]}
+										/>
 									</div>
 								</div>
-								<div className="d-flex align-items-center justify-content-end gap-4">
-									<MdKeyboardArrowLeft
-										size={20}
-										className={`${privateClasses.paginationArrow}`}
-									/>
-									<p
-										className={`${privateClasses.paginationNumber} ${privateClasses.paginationNumberActive}`}
-									>
-										1
-									</p>
-									<p className={`${privateClasses.paginationNumber}`}>2</p>
-									<MdKeyboardArrowRight
-										size={20}
-										className={`${privateClasses.paginationArrow}`}
-									/>
-								</div>
+								<TablePagination />
 							</div>
 						</div>
 					</div>
